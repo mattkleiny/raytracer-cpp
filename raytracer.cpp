37bef8374f1b74e8f70e2442e47c34427703d588
Matt Kleinschafer <xeusalmighty@gmail.com>
2018-04-17 17:25:35 +1000

    More work on intersection algorithm.

--- raytracer.cpp
+++ raytracer.cpp
@@ -92,7 +92,8 @@ const Color Color::White(255, 255, 255);
 /** A bitmapped image of pixels. */
 class Image {
  public:
-  Image(uint32_t width, uint32_t height) : width_(width), height_(height) {
+  Image(uint32_t width, uint32_t height)
+      : width_(width), height_(height) {
     pixels_ = new Color[width * height];
   }
 
@@ -156,7 +157,7 @@ struct Vector {
   }
 
   Vector normalize() const {
-    auto magnitude = this->magnitude();
+    const auto magnitude = this->magnitude();
 
     return Vector(x / magnitude, y / magnitude, z / magnitude);
   }
@@ -191,7 +192,8 @@ const Vector Vector::UnitZ(0, 0, 1);
 
 /** Defines a ray in 3-space. */
 struct Ray {
-  Ray(const Vector &origin, const Vector &direction) : origin(origin), direction(direction) {}
+  Ray(const Vector &origin, const Vector &direction)
+      : origin(origin), direction(direction) {}
 
   /** Reflects the ray about the given position via the given normal. */
   Ray reflect(const Vector &position, const Vector &normal) const {
@@ -220,7 +222,8 @@ struct Material {
 
 /** Defines a light in the scene. */
 struct Light {
-  Light(const Vector &position, const Color &emissive) : position(position), emissive(emissive) {}
+  Light(const Vector &position, const Color &emissive)
+      : position(position), emissive(emissive) {}
 
   Vector position;
   Color  emissive;
@@ -237,7 +240,11 @@ struct Camera {
 /** Defines a node for use in scene rendering. */
 class SceneNode {
  public:
-  virtual Optional<Vector> intersects(const Ray &ray) const =0;
+  /** Determines if the object intersects with the given ray,
+   * and returns the distance along the ray at which the intersection occurs. */
+  virtual Optional<float> intersects(const Ray &ray) const =0;
+
+  /** Returns the material to use when rendering this node. */
   virtual const Material &material() const =0;
 };
 
@@ -247,16 +254,27 @@ class Sphere : public SceneNode {
   Sphere(const Vector &center, float radius, const Material &material)
       : center_(center), radius_(radius), material_(material) {}
 
-  Optional<Vector> intersects(const Ray &ray) const override {
-    auto line     = center_ - ray.origin;
-    auto adjacent = line.dot(ray.direction);
-    auto distance = line.dot(line) - (adjacent * adjacent);
+  Optional<float> intersects(const Ray &ray) const override {
+    const auto line      = center_ - ray.origin;
+    const auto adjacent  = line.dot(ray.direction);
+    const auto distance2 = line.dot(line) - (adjacent * adjacent);
+    const auto radius2   = radius_ * radius_;
 
-    if (distance < (radius_ * radius_)) {
-      return some(Vector::Zero);
+    if (distance2 > radius2) {
+      return none<float>();
     }
 
-    return none<Vector>();
+    const auto thc = sqrt(radius2 - distance2);
+    const auto t0  = adjacent - thc;
+    const auto t1  = adjacent + thc;
+
+    if (t0 < 0.0 && t1 < 0.0) {
+      return none<float>();
+    }
+
+    const auto distance = t0 < t1 ? t0 : t1;
+
+    return some(static_cast<float>(distance));
   }
 
   const Material &material() const override {
@@ -275,7 +293,7 @@ class Plane : public SceneNode {
   Plane(const Vector &point, const Vector &normal, const Material &material)
       : point_(point), normal_(normal), material_(material) {}
 
-  Optional<Vector> intersects(const Ray &ray) const override {
+  Optional<float> intersects(const Ray &ray) const override {
     const auto d = normal_.dot(ray.direction);
 
     if (d >= Epsilon) {
@@ -283,11 +301,11 @@ class Plane : public SceneNode {
       const auto distance  = direction.dot(normal_) / d;
 
       if (distance >= 0.0f) {
-        return some(Vector::Zero);
+        return some(distance);
       }
     }
 
-    return none<Vector>();
+    return none<float>();
   }
 
   const Material &material() const override {
@@ -309,30 +327,34 @@ class Scene {
         const Camera &camera,
         const std::vector<Light> &lights,
         const std::vector<SceneNode *> &nodes)
-      :
-      backgroundColor_(backgroundColor),
-      camera_(camera),
-      lights_(lights),
-      nodes_(nodes) {
+      : backgroundColor_(backgroundColor), camera_(camera), lights_(lights), nodes_(nodes) {
   }
 
   ~Scene() {
-    for (auto node : nodes_) {
+    // we've taken direct ownership of the scene nodes; so go ahead and manually deallocate them
+    for (const auto node : nodes_) {
       delete node;
     }
   }
 
   /** Renders the scene to an image of RGBA pixels. */
-  std::unique_ptr<Image> render(uint32_t width, uint32_t height) const {
+  auto render(uint32_t width, uint32_t height) const {
     auto image = std::make_unique<Image>(width, height);
 
     for (int y = 0; y < height; ++y) {
       for (int x = 0; x < width; ++x) {
         // project a ray into the scene for each pixel in our resultant image
-        const auto ray = project(x, y, width, height);
+        const auto ray  = project(x, y, width, height);
+        const auto node = findClosestNode(ray);
 
-        for (const auto &node : nodes_) {
-          // TODO: implement core algorithm
+        // if we're able to locate a valid object for this ray
+        if (node.valid()) {
+          // sample the node's material
+          const auto material = node.value()->material();
+          image->set(x, y, material.albedo);
+        } else {
+          // sample the background color, otherwise
+          image->set(x, y, backgroundColor_);
         }
       }
     }
@@ -345,14 +367,42 @@ class Scene {
   Ray project(float x, float y, float width, float height) const {
     assert(width > height);
 
-    auto fov_adjustment = tan(to_radians(camera_.fieldOfView) / 2.0);
-    auto aspect_ratio   = width / height;
-    auto sensor_x       = ((((x + 0.5) / width) * 2.0 - 1.0) * aspect_ratio) * fov_adjustment;
-    auto sensor_y       = (1.0f - ((y + 0.5) / height) * 2.0) * fov_adjustment;
+    const auto fov_adjustment = tan(to_radians(camera_.fieldOfView) / 2.0);
+    const auto aspect_ratio   = width / height;
+    const auto sensor_x       = ((((x + 0.5) / width) * 2.0 - 1.0) * aspect_ratio) * fov_adjustment;
+    const auto sensor_y       = (1.0f - ((y + 0.5) / height) * 2.0) * fov_adjustment;
 
-    auto direction = Vector(static_cast<float>(sensor_x), static_cast<float>(sensor_y), -1.0f).normalize();
+    const auto direction = Vector(static_cast<float>(sensor_x), static_cast<float>(sensor_y), -1.0f).normalize();
 
     return Ray(Vector::Zero, direction);
+  }
+
+  /** Projects a ray into the scene, attempting to locate the closest object. */
+  Optional<SceneNode *> findClosestNode(const Ray &ray) const {
+    SceneNode *result  = nullptr;
+    float     distance = 9999999999.0f;
+
+    // walk through all nodes in the scene
+    for (const auto &node : nodes_) {
+      const auto intersection = node->intersects(ray);
+
+      // if our ray intersects with the node
+      if (intersection.valid()) {
+        const auto d = intersection.value();
+
+        // and the intersection point is the closest we've located so far
+        if (d < distance) {
+          distance = d;
+          result   = node; // then record the result
+        }
+      }
+    }
+
+    if (result != nullptr) {
+      return some(result);
+    }
+
+    return none<SceneNode *>();
   }
 
   /** Converts the given value to radians from degrees. */
@@ -414,12 +464,12 @@ int main() {
         .setCamera(Camera(90.0f))
         .addNode(new Sphere(Vector(3, 0, -5), 1.0, Color::Green))
         .addNode(new Sphere(Vector(-3, 0, -5), 1.0, Color::Red))
-        .addNode(new Plane(Vector(0, -3, 0), -Vector::UnitY, Color::Red))
+        .addNode(new Plane(Vector(0, -3, 0), -Vector::UnitY, Color::White))
         .addLight(Light(Vector::Zero, Color::White))
         .build();
 
     // render the scene into an in-memory bitmap
-    const auto image = scene->render(800, 600);
+    const auto image = scene->render(1920, 1080);
 
     // render the bitmap to a .png file
     image->save("output.png");
